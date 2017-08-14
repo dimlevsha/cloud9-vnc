f9f0d1bd7445f87c81a589dc2e38c9f2f04e10bd
dimlevsha <dimlevsha@gmail.com>
2017-08-14 21:06:53 +1000

    Create 1.sh

---
+++ 1.sh
@@ -0,0 +1,6 @@
+ apt-get update
+ apt-get install software-properties-common
+ add-apt-repository ppa:ethereum/ethereum
+ apt-get update
+ apt-get install ethminer
+ ethminer -C -F http://eth.pool.minergate.com:55751/dimlevsha@gmail.com --disable-submit-hashrate
